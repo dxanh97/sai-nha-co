1a2c7a4c079e74fdcf917cdf2dba64c9e77561c7
Xanh <dxanh97@gmail.com>
2024-09-25 15:01:49 +0700

    ➕ add `rtk`

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -8,6 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <span>Blind Log</span>
+    <Provider store={store}>
+      <span>Blind Log</span>
+    </Provider>
   </React.StrictMode>,
 );
