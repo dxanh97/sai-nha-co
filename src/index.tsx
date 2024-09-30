772dc62c9a46c96015e3ed5451bc9c5091e616bc
Xanh <dxanh97@gmail.com>
2024-09-30 22:43:46 +0700

    ➕ add `sass`

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <span>Blind Log</span>
+      <span className="logo">📒</span>
     </Provider>
   </React.StrictMode>,
 );
