61d892a3cab8a748e07dd2890a8b27f501050d7f
Xanh <dxanh97@gmail.com>
2024-10-07 03:26:16 +0700

    💄 remove index.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { Container, createTheme, MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
@@ -11,8 +11,6 @@ import { store } from './redux/store';
 import HomePage from './pages/home';
 import NewGamePage from './pages/new-game';
 import GameScreenPage from './pages/game-screen';
-
-import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -27,21 +25,23 @@ const theme = createTheme({
 });
 
 root.render(
-  <MantineProvider theme={theme}>
+  <MantineProvider theme={theme} defaultColorScheme="auto">
     <ModalsProvider>
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={null} // layout component
-            >
-              <Route index element={<HomePage />} />
-              <Route path="/new-game" element={<NewGamePage />} />
-              <Route path="/game/:gameId" element={<GameScreenPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <Container p="sm">
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={null} // layout component
+              >
+                <Route index element={<HomePage />} />
+                <Route path="/new-game" element={<NewGamePage />} />
+                <Route path="/game/:gameId" element={<GameScreenPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </Container>
       </Provider>
     </ModalsProvider>
   </MantineProvider>,
