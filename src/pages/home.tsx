52e12ec25bdc1bfb66add286e3caac3f98c4938c
Xanh <dxanh97@gmail.com>
2024-10-07 18:13:45 +0700

    ✨ re-implement home screen

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,55 @@
-import { Box, Button, Center, Stack, Text } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import { ActionIcon, Box, Card, Group, Text } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+
+import { useAppSelector } from '../redux/store';
+import { selectAllGames } from '../redux/game.selector';
+
+import TopNav from '../components/shared/TopNav';
+import Empty from '../components/shared/Empty';
+import PlayerAvatars from '../components/game-screen/PlayerAvatars';
+import { formatDateTime } from '../utils/helpers';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const allGames = useAppSelector(selectAllGames);
+
   return (
-    <Stack h="100%">
-      <Box ta="center">
-        <Center h="200px">
-          <Text size="100px">📒</Text>
-        </Center>
-      </Box>
-      <Link to="/new-game">
-        <Button variant="light" fullWidth>
-          New Game
-        </Button>
-      </Link>
-      <Link to="/history">
-        <Button variant="light" fullWidth>
-          History
-        </Button>
-      </Link>
-    </Stack>
+    <Box>
+      <TopNav isHome title="Bet Log" />
+
+      {allGames.length === 0 && (
+        <Empty emoji="👀" subTitle="Create a game and start logging" />
+      )}
+      {allGames.map((x) => (
+        <Card
+          key={x.id}
+          shadow="sm"
+          mb="sm"
+          onClick={() => navigate(`/game/${x.id}`)}
+        >
+          <Group justify="space-between">
+            <Text fw={800}>{x.name}</Text>
+            <Text>🗑️</Text>
+          </Group>
+          <Text c="dimmed">{formatDateTime(x.timestamp)}</Text>
+          <PlayerAvatars playerNames={x.playerNames} />
+        </Card>
+      ))}
+      <ActionIcon
+        variant="light"
+        size="xl"
+        radius="xl"
+        pos="fixed"
+        bottom={20}
+        right={20}
+        onClick={() => navigate('/new-game')}
+      >
+        <Text fz="xl" lh={0}>
+          ➕
+        </Text>
+      </ActionIcon>
+    </Box>
   );
 }
 
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,3 +13,15 @@ export const getSum = (list: number[]) => {
   const sum = list.reduce((acc, cur) => acc + cur, 0);
   return sum;
 };
+
+export const formatDateTime = (t: number) => {
+  const date = new Date(t);
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  const hh = date.getHours();
+  const mm = date.getMinutes();
+  const dateString = `${d < 10 ? `0${d}` : d}-${m}-${y}`;
+  const timeString = `${hh < 10 ? `0${hh}` : hh}:${mm < 10 ? `0${mm}` : mm}`;
+  return `${dateString} ${timeString}`;
+};
